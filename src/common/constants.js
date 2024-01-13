d2bbe61be60eef74f4893aa36b93033d39202edb
Surya Sonar <iamsuryasonar@gmail.com>
2024-01-13 16:00:24 +0530

    deploy: netlify configured

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,2 +1,3 @@
 export const API_URL = 'http://localhost:3001/admin/api/v1/';
+// export const API_URL = 'https://limo-backend-e2jw.onrender.com/admin/api/v1/'
 export const LOCAL_STORAGE_NAME = 'limoADMIN';
