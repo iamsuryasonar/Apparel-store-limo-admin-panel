d829b95e8be27254dc76f947f44f8b16bda15e6c
Surya Sonar <iamsuryasonar@gmail.com>
2024-11-22 12:58:50 +0530

    guest log in creds added

--- src/pages/auth/LogInPage.jsx
+++ src/pages/auth/LogInPage.jsx
@@ -9,7 +9,10 @@ function LogInPage() {
     const navigate = useNavigate();
     // const { message } = useSelector((state) => state.message);
 
-    const [input, setInput] = useState({});
+    const [input, setInput] = useState({
+        email: '',
+        password: '',
+    });
 
     useEffect(() => {
         dispatch(clearMessage());
@@ -31,6 +34,14 @@ function LogInPage() {
             })
     }
 
+    function handleAutoFillGuestCreds() {
+        setInput({
+            email: 'admin@limostore.com',
+            password: '11111111',
+        })
+    }
+
+
     return (<>
         {/* {message && <BottomAlert message={message} />} */}
         <div className='w-full flex justify-center'>
@@ -39,16 +50,19 @@ function LogInPage() {
                 <p className="font-light text-lg">Don't have an account? <Link to='/sign-up' className="underline">Sign up here</Link></p>
                 <div className="w-full flex flex-col justify-center items-center gap-4">
                     <form className="w-full flex flex-col gap-4 font-light ">
-                        <input onChange={onChangeHandler} type="email" name='email' placeholder='Email' className="p-1 border-[1px] rounded-sm border-black w-full placeholder:p-2 "></input>
-                        <input onChange={onChangeHandler} type="password" name='password' placeholder='Pasword' className="p-1 border-[1px] rounded-sm border-black w-full placeholder:p-2 "></input>
+                        <input onChange={onChangeHandler} value={input.email} type="email" name='email' placeholder='Email' className="p-1 border-[1px] rounded-sm border-black w-full placeholder:p-2 "></input>
+                        <input onChange={onChangeHandler} value={input.password} type="password" name='password' placeholder='Pasword' className="p-1 border-[1px] rounded-sm border-black w-full placeholder:p-2 "></input>
                     </form>
                     <div className='w-full flex justify-between items-center'>
                         <div className="flex gap-4">
                             <button onClick={logInHandler} className="px-6 py-2 bg-black text-white font-light">Sign In</button>
-                            {/* <a href="" className="underline font-light">Return to store</a> */}
                         </div>
                         <a href="" className="underline font-light self-start">Forgot Password</a>
                     </div>
+                    <button
+                        className="bg-slate-950 text-white px-2 py-1 place-self-end"
+                        onClick={handleAutoFillGuestCreds}>Auto fill guest credentials
+                    </button>
                 </div>
             </div>
         </div >
